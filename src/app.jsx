4a5bc9d25626808b8600ad20dcc1a08aa955f59f
Wenceslao José Villegas <wenceslaovillegas14@gmail.com>
2022-03-18 15:06:20 -0300

    basic components sintax

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from "./shared/header/header"
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <h1>Titulo</h1>
+            <div>App</div>
+        </>
+    )
+}
---
+++ src/components/Listado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Listado = () => {
+    return (
+        <ul>
+            <li>Lista 1</li>
+            <li>Lista 2</li>
+            <li>Lista 3git pu</li>
+        </ul>
+    )
+}
